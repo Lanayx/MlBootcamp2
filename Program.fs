a08fb2d6d1d4bc8eb108f95052f3f6e5f7e3fa07
Vladimir Shchur <shchurvladimir@gmail.com>
2018-11-11 20:25:57 +0300

    Added normalized csv

--- Program.fs
+++ Program.fs
@@ -1,8 +1,34 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open FSharp.Data
+open System.Globalization
+open Microsoft.ML
+open Microsoft.ML.Core.Data
+open Microsoft.ML.Runtime.Api
+open Microsoft.ML.Runtime.Data
+open Microsoft.ML.Transforms.Text
+
+
+type Normalized = CsvProvider<"../data/train/normalized.csv",",">
+
+let userData = new Normalized()
 
 [<EntryPoint>]
 let main argv =
+
+    let mlContext = MLContext()
+
+    let _textLoader = mlContext.Data.TextReader(TextLoader.Arguments
+                        (
+
+                            Separator = ",",
+                            HasHeader = true,
+                            Column =    [|
+                                          TextLoader.Column("Label", Nullable(DataKind.Bool), 0)
+                                          TextLoader.Column("COM_CAT#1", Nullable(DataKind.Num), 1)
+                                        |]
+                        ))
+
     printfn "Hello World from F#!"
     0 // return an integer exit code
