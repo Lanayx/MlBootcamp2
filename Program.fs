6bfd20a071a4615d9167dd96202c9a2cf768b6e5
Odin <odinko@odin.com>
2018-11-12 18:59:54 +0300

    Some tries

--- Program.fs
+++ Program.fs
@@ -8,11 +8,13 @@ open Microsoft.ML.Core.Data
 open Microsoft.ML.Runtime.Api
 open Microsoft.ML.Runtime.Data
 open Microsoft.ML.Transforms.Text
+open Microsoft.ML.Trainers
+open Microsoft.ML.Transforms
 
 
-type Normalized = CsvProvider<"../data/train/normalized.csv",",">
+//type Normalized = CsvProvider<"normalized.csv",",">
 
-let userData = new Normalized()
+//let userData = new Normalized()
 
 [<EntryPoint>]
 let main argv =
@@ -25,10 +27,24 @@ let main argv =
                             Separator = ",",
                             HasHeader = true,
                             Column =    [|
-                                          TextLoader.Column("Label", Nullable(DataKind.Bool), 0)
-                                          TextLoader.Column("COM_CAT#1", Nullable(DataKind.Num), 1)
+                                          TextLoader.Column("Label", Nullable(DataKind.BL), 0)
+                                          TextLoader.Column("CategoricalFeatures", Nullable(DataKind.TX), 1, 10)
+                                          TextLoader.Column("NumericalFeatures", Nullable(DataKind.R4), 11, 17)
                                         |]
                         ))
+
+    let data = _textLoader.Read("normalized.csv")
+    let classification = new BinaryClassificationContext(mlContext)
+
+    let est = ColumnConcatenatingEstimator(mlContext, "Features", "CategoricalFeatures", "NumericalFeatures")
+    let struct(train, test) = classification.TrainTestSplit(data, testFraction = 0.2)
+    
+    est.Append(LinearClassificationTrainer(mlContext, "Features", "Label"))
+        
+    let model = est.Fit(train)
+    let predictions = model.Transform(test);
+    let evaluationResult = classification.Evaluate(predictions, "Label")
+    //classification.CrossValidate(train, est)
 
     printfn "Hello World from F#!"
     0 // return an integer exit code
