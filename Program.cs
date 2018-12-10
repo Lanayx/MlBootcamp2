efe660ac22116261de3c8228202db5176e62a6db
Odin <odinko@odin.com>
2018-12-10 10:27:30 +0300

    Adapted for k-means

--- Program.cs
+++ Program.cs
@@ -9,28 +9,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.ML.Runtime.Api;
+using Microsoft.ML.Transforms.Normalizers;
 
 namespace ConsoleApp15
 {
 
-    public class Input
-    {
-        [Column(ordinal: "0", name: "Label")]
-        public float Label;
-        [Column(ordinal: "1")]
-        public string CategoricalFeatures;
-    }
-
-    public class Output
+    public class ClusteringPrediction
     {
         [ColumnName("PredictedLabel")]
-        public bool Prediction { get; set; }
-
-        [ColumnName("Probability")]
-        public float Probability { get; set; }
-
+        public uint SelectedClusterId;
         [ColumnName("Score")]
-        public float Score { get; set; }
+        public float[] Distance;
+        [ColumnName("Id")]
+        public string Id;
     }
 
 
@@ -48,38 +39,56 @@ namespace ConsoleApp15
                 HasHeader = true,
                 Column = new[]
                 {
-                        new TextLoader.Column("Label", DataKind.BL, 0),
-                        new TextLoader.Column("CategoricalFeatures", DataKind.TX, 1, 10),
-                        new TextLoader.Column("NumericalFeatures", DataKind.R4, 11, 17)
+                        new TextLoader.Column("Id", DataKind.TX, 0),
+                        new TextLoader.Column("NumericalFeatures", DataKind.R4, 1, 75)
                 }
             }
                                 );
 
             // Read the data.
-            var data = reader.Read("../../../normalized.csv");  
+            var data = reader.Read("../../../kpi.csv");
+            for (var i = 5; i <= 40; i++)
+            {
 
-            // Build several alternative featurization pipelines.
-            var dynamicPipeline =
-                // Convert each categorical feature into one-hot encoding independently.
-                mlContext.Transforms.Categorical.OneHotEncoding("CategoricalFeatures", "CategoricalOneHot");
+                // Build several alternative featurization pipelines.
+                var dynamicPipeline =
+                    // Convert each categorical feature into one-hot encoding independently.
+                    mlContext.Transforms.Normalize("NumericalFeatures", mode: NormalizingEstimator.NormalizerMode.LogMeanVariance);
 
-            // Of course, if we want to train the model, we will need to compose a single float vector of all the features.
-            // Here's how we could do this:
+                var trainer = mlContext.Clustering.Trainers.KMeans("NumericalFeatures", clustersCount: i);
 
-            var trainer = mlContext.BinaryClassification.Trainers.FastTree("Label", "CategoricalOneHot", numTrees: 20);
+                var fullLearningPipeline = dynamicPipeline
+                    // Now we're ready to train. We chose our FastTree trainer for this classification task.
+                    .Append(trainer);
 
-            var fullLearningPipeline = dynamicPipeline
-                // Concatenate two of the 3 categorical pipelines, and the numeric features.
-                .Append(mlContext.Transforms.Concatenate("Features", "CategoricalOneHot"))
-                // Now we're ready to train. We chose our FastTree trainer for this classification task.
-                .Append(trainer);
+                // Train the model.
+                // var (train, test) = mlContext.Clustering.TrainTestSplit(data, testFraction: 0.5);
+                var model = fullLearningPipeline.Fit(data);
+                var predictions = model.Transform(data);
+                var evaluationResult = mlContext.Clustering.Evaluate(predictions);
+                Console.WriteLine("AvgMinScore = {0}, Dbi={1}, Nmi={2} i = {3}",
+                    evaluationResult.AvgMinScore, evaluationResult.Dbi, evaluationResult.Nmi, i);
+               
+            }
+            //var results =
+            //       predictions.AsEnumerable<ClusteringPrediction>(mlContext, false).ToArray();
+            //SaveSegmentationCSV(results, "clustering.csv");
+        }
 
-            // Train the model.
-            var (train, test) = mlContext.BinaryClassification.TrainTestSplit(data, testFraction: 0.5);
-            var model = fullLearningPipeline.Fit(train);
-            var predictions = model.Transform(test);
-            var evaluationResult = mlContext.BinaryClassification.Evaluate(predictions, "Label");
-            Console.WriteLine("Auc = {0}, Trainer = {1}",evaluationResult.Auc, train.GetType().Name);
+        private static void SaveSegmentationCSV(ClusteringPrediction[] predictions, string csvlocation)
+        {
+            
+            using (var w = new System.IO.StreamWriter(csvlocation))
+            {
+                w.WriteLine($"CELL_LAC_ID,CLUSTER_NUM");
+                w.Flush();
+                predictions.ToList().ForEach(prediction => {
+                    w.WriteLine($"{prediction.Id},{prediction.SelectedClusterId}");
+                    w.Flush();
+                });
+            }
+
+            Console.WriteLine($"CSV location: {csvlocation}");
         }
     }
 }
